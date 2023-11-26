1500484ec004274c73df2cee48e769410764272e
designxcel <designxcel08@gmail.com>
2023-11-26 21:28:39 +0600

    added few endpoint routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -31,6 +31,8 @@ async function run() {
     const reviewsCollection = client.db("techMed").collection("reviews")
     const doctorsCollection = client.db("techMed").collection("drlists")
     const centerCollection = client.db("techMed").collection("center")
+    const userCollection = client.db("techMed").collection("users")
+    const testCollection = client.db("techMed").collection("test")
 
     //for getting all blogs data endpoint
     app.get('/blogs', async(req,res) => {
@@ -50,9 +52,70 @@ async function run() {
       res.send(result)
     })
 
+    //for posting doctor info data endpoint
+    app.post('/drlists', async(req, res) => {
+      const item = req.body
+      const result = await doctorsCollection.insertOne(item);
+      res.send(result)
+    })
+
     //for getting center list data endpoint
     app.get('/center', async(req, res) => {
       const result = await centerCollection.find().toArray()
+      res.send(result)
+    })
+
+    //for getting all tests list data endpoint
+    app.get('/test', async(req, res) => {
+      const result = await testCollection.find().toArray()
+      res.send(result)
+    })
+
+    //for getting test details data endpoint
+    app.get('/test/:id', async(req,res) =>{
+      const id = req.params.id;
+      // console.log(id)
+      const query = {_id: new ObjectId(id)}
+      const result = await testCollection.findOne(query)
+      res.send(result)
+    })
+
+    //for getting all users data endpoint
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
+    //for post users data endpoint
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const query = {email:user?.email}
+      const existingUser = await userCollection.findOne(query)
+      if(existingUser){
+        return res.send({message:'User already exist', insertedId:null})
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+    //for updating user data endpoint
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+          $set: {
+            role: 'admin'
+          }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+    //for user deleting data endpoint
+    app.delete('/users/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
       res.send(result)
     })
 
